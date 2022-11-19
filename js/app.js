f1920d675505872dfaa481e9e479349dfb25b729
kamal00yyu <52659066+kamal00yyu@users.noreply.github.com>
2022-11-19 17:55:05 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function onClickMenu() {
+    document.getElementById("menu-hamberger").classList.toggle("change");
+    document.getElementById("nav-hamberger").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+
+}
